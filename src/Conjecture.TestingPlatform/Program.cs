a6319ac5b3979e4c970674f63d40b9824ceadcd3
Kim Ommundsen <kommundsen@users.noreply.github.com>
2026-04-19 12:50:30 +0200

    Adds ConjectureTestingPlatformExtensions with RegisterConjectureFramework extension method

--- src/Conjecture.TestingPlatform/Program.cs
+++ src/Conjecture.TestingPlatform/Program.cs
@@ -1,14 +1,11 @@
 // Copyright (c) 2026 Kim Ommundsen. Licensed under the MPL-2.0.
 // See LICENSE.txt in the project root or https://mozilla.org/MPL/2.0/
 
-using Conjecture.TestingPlatform.Internal;
+using Conjecture.TestingPlatform;
 
 using Microsoft.Testing.Platform.Builder;
 
 ITestApplicationBuilder builder = await TestApplication.CreateBuilderAsync(args);
-builder.CommandLine.AddProvider(static () => new ConjectureCommandLineOptions());
-builder.RegisterTestFramework(
-    _ => new PropertyTestFrameworkCapabilities(),
-    (_, services) => new PropertyTestFramework(services));
+builder.RegisterConjectureFramework();
 using ITestApplication app = await builder.BuildAsync();
 return await app.RunAsync();
