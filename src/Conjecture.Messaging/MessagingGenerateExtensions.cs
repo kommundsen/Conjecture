5ba5dbb5a9aeee716992fa22d2afb30028476422
Kim Ommundsen <kommundsen@users.noreply.github.com>
2026-04-26 01:42:58 +0200

    Add Generate.Messaging.Publish / Consume strategy verbs
    Closes #464
    Part of #450

---
+++ src/Conjecture.Messaging/MessagingGenerateExtensions.cs
@@ -0,0 +1,16 @@
+// Copyright (c) 2026 Kim Ommundsen. Licensed under the MPL-2.0.
+// See LICENSE.txt in the project root or https://mozilla.org/MPL/2.0/
+
+using Conjecture.Core;
+
+namespace Conjecture.Messaging;
+
+/// <summary>Extension methods on <see cref="Generate"/> for messaging interaction generation.</summary>
+public static class MessagingGenerateExtensions
+{
+    extension(Generate)
+    {
+        /// <summary>Returns a <see cref="MessagingGenerateBuilder"/> for composing message interaction strategies.</summary>
+        public static MessagingGenerateBuilder Messaging => new();
+    }
+}
