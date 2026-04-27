c4cd8d5e5d72961afdc7196dbdf3ceb171a8b9d3
Kim Ommundsen <kommundsen@users.noreply.github.com>
2026-04-27 02:37:44 +0200

    EFCoreGenerate: extension block factory methods on Generate
    Closes #398
    Part of #291

---
+++ src/Conjecture.EFCore/EFCoreGenerateExtensions.cs
@@ -0,0 +1,43 @@
+// Copyright (c) 2026 Kim Ommundsen. Licensed under the MPL-2.0.
+// See LICENSE.txt in the project root or https://mozilla.org/MPL/2.0/
+
+using System;
+
+using Conjecture.Core;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Conjecture.EFCore;
+
+/// <summary>Factory methods for generating EF Core entity instances.</summary>
+public static class EFCoreGenerateExtensions
+{
+    // Both overloads take optional maxDepth; suppress RS0026 as in RegexGenerateExtensions.
+#pragma warning disable RS0026
+    extension(Generate)
+    {
+        /// <summary>Returns a strategy that generates instances of <typeparamref name="T"/> using the given <paramref name="context"/>.</summary>
+        public static Strategy<T> Entity<T>(DbContext context, int maxDepth = 2) where T : class
+        {
+            ArgumentNullException.ThrowIfNull(context);
+            return new EntityStrategyBuilder(context.Model)
+                .WithMaxDepth(maxDepth)
+                .Build<T>();
+        }
+
+        /// <summary>Returns a strategy that generates instances of <typeparamref name="T"/> using a fresh <see cref="DbContext"/> per example.</summary>
+        public static Strategy<T> Entity<T>(Func<DbContext> contextFactory, int maxDepth = 2) where T : class
+        {
+            ArgumentNullException.ThrowIfNull(contextFactory);
+            return Generate.Compose<T>(ctx =>
+            {
+                using DbContext context = contextFactory();
+                Strategy<T> inner = new EntityStrategyBuilder(context.Model)
+                    .WithMaxDepth(maxDepth)
+                    .Build<T>();
+                return ctx.Generate(inner);
+            });
+        }
+    }
+#pragma warning restore RS0026
+}
