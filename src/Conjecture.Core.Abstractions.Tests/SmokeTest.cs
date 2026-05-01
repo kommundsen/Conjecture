a67b18f7176b1c406b799ffe1ce4e7f9b2337085
Kim Ommundsen <kommundsen@users.noreply.github.com>
2026-05-01 10:38:49 +0200

    Adds paired test projects for Abstractions packages

---
+++ src/Conjecture.Core.Abstractions.Tests/SmokeTest.cs
@@ -0,0 +1,12 @@
+// Copyright (c) 2026 Kim Ommundsen. Licensed under the MPL-2.0.
+// See LICENSE.txt in the project root or https://mozilla.org/MPL/2.0/
+
+namespace Conjecture.Core.Abstractions.Tests;
+
+public class SmokeTest
+{
+    [Fact]
+    public void Package_loads()
+    {
+    }
+}
