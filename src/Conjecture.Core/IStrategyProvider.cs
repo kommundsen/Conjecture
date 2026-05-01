7c46dfd58095e6387bf637fcb7a9cc74388ba78a
Kim Ommundsen <kommundsen@users.noreply.github.com>
2026-05-01 11:09:50 +0200

    Hides IStrategyProvider<T> from end-user IntelliSense via EditorBrowsable

--- src/Conjecture.Core/IStrategyProvider.cs
+++ src/Conjecture.Core/IStrategyProvider.cs
@@ -1,14 +1,17 @@
 // Copyright (c) 2026 Kim Ommundsen. Licensed under the MPL-2.0.
 // See LICENSE.txt in the project root or https://mozilla.org/MPL/2.0/
 
+using System.ComponentModel;
 
 namespace Conjecture.Core;
 
 /// <summary>Non-generic marker interface used as a generic constraint on <see cref="FromAttribute{TProvider}"/>.</summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
 public interface IStrategyProvider { }
 
 /// <summary>Provides a <see cref="Strategy{T}"/> instance for use in property-based tests.</summary>
 /// <typeparam name="T">The type of value the strategy generates.</typeparam>
+[EditorBrowsable(EditorBrowsableState.Never)]
 public interface IStrategyProvider<T> : IStrategyProvider
 {
     /// <summary>Creates and returns a strategy for generating <typeparamref name="T"/> values.</summary>
