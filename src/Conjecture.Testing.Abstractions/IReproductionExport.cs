80cfbb77bd4b6b3e413eb88d5baa05cdf5375e05
Kim Ommundsen <kommundsen@users.noreply.github.com>
2026-05-01 11:22:48 +0200

    Moves IReproductionExport from Conjecture.Core to Conjecture.Testing.Abstractions

--- src/Conjecture.Testing.Abstractions/IReproductionExport.cs
+++ src/Conjecture.Testing.Abstractions/IReproductionExport.cs
@@ -1,15 +1,16 @@
 // Copyright (c) 2026 Kim Ommundsen. Licensed under the MPL-2.0.
 // See LICENSE.txt in the project root or https://mozilla.org/MPL/2.0/
 
-using Conjecture.Abstractions.Testing;
+using System.ComponentModel;
 
-namespace Conjecture.Core;
+namespace Conjecture.Abstractions.Testing;
 
 /// <summary>
 /// Optional interface for <c>[Property]</c> attributes that support exporting
 /// reproduction files on test failure. Implement alongside <see cref="IPropertyTest"/>
 /// to opt in to this capability.
 /// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
 public interface IReproductionExport
 {
     /// <summary>Whether to export a reproduction file when the test fails.</summary>
