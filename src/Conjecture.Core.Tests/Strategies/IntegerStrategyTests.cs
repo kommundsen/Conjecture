b2c07fd9ff923fb5e8e21485c4f40993a28418eb
Kim Ommundsen <kommundsen@users.noreply.github.com>
2026-05-01 17:44:01 +0200

    Converts IntegerStrategyTests to use public Sample API and [Property]

--- src/Conjecture.Core.Tests/Strategies/IntegerStrategyTests.cs
+++ src/Conjecture.Core.Tests/Strategies/IntegerStrategyTests.cs
@@ -2,78 +2,50 @@
 // See LICENSE.txt in the project root or https://mozilla.org/MPL/2.0/
 
 using Conjecture.Core;
-using Conjecture.Core.Internal;
 
 namespace Conjecture.Core.Tests.Strategies;
 
 public class IntegerStrategyTests
 {
-    private static ConjectureData MakeData(ulong seed = 42UL) =>
-        ConjectureData.ForGeneration(new SplittableRandom(seed));
-
     [Fact]
     public void Integers_DefaultRange_Int_InRange()
     {
-        var strategy = Strategy.Integers<int>();
-        var data = MakeData();
-        for (var i = 0; i < 100; i++)
-        {
-            Assert.InRange(strategy.Generate(data), int.MinValue, int.MaxValue);
-        }
+        Strategy<int> strategy = Strategy.Integers<int>();
+        Assert.All(strategy.WithSeed(42UL).Sample(100), v => Assert.InRange(v, int.MinValue, int.MaxValue));
     }
 
     [Fact]
     public void Integers_BoundedRange_ReturnsInRange()
     {
-        var strategy = Strategy.Integers<int>(0, 9);
-        var data = MakeData();
-        for (var i = 0; i < 1000; i++)
-        {
-            Assert.InRange(strategy.Generate(data), 0, 9);
-        }
+        Strategy<int> strategy = Strategy.Integers<int>(0, 9);
+        Assert.All(strategy.WithSeed(1UL).Sample(1000), v => Assert.InRange(v, 0, 9));
     }
 
     [Fact]
     public void Integers_MinEqualsMax_ReturnsConstant()
     {
-        var strategy = Strategy.Integers<int>(5, 5);
-        var data = MakeData();
-        for (var i = 0; i < 20; i++)
-        {
-            Assert.Equal(5, strategy.Generate(data));
-        }
+        Strategy<int> strategy = Strategy.Integers<int>(5, 5);
+        Assert.All(strategy.WithSeed(1UL).Sample(5), v => Assert.Equal(5, v));
     }
 
     [Fact]
     public void Integers_NegativeRange_ReturnsInRange()
     {
-        var strategy = Strategy.Integers<int>(-10, -1);
-        var data = MakeData();
-        for (var i = 0; i < 100; i++)
-        {
-            Assert.InRange(strategy.Generate(data), -10, -1);
-        }
+        Strategy<int> strategy = Strategy.Integers<int>(-10, -1);
+        Assert.All(strategy.WithSeed(1UL).Sample(100), v => Assert.InRange(v, -10, -1));
     }
 
     [Fact]
     public void Integers_LongRange_ReturnsInRange()
     {
-        var strategy = Strategy.Integers<long>(0L, 100L);
-        var data = MakeData();
-        for (var i = 0; i < 100; i++)
-        {
-            Assert.InRange(strategy.Generate(data), 0L, 100L);
-        }
+        Strategy<long> strategy = Strategy.Integers<long>(0L, 100L);
+        Assert.All(strategy.WithSeed(1UL).Sample(100), v => Assert.InRange(v, 0L, 100L));
     }
 
     [Fact]
     public void Integers_ByteRange_ReturnsInRange()
     {
-        var strategy = Strategy.Integers<byte>(0, 10);
-        var data = MakeData();
-        for (var i = 0; i < 100; i++)
-        {
-            Assert.InRange(strategy.Generate(data), (byte)0, (byte)10);
-        }
+        Strategy<byte> strategy = Strategy.Integers<byte>(0, 10);
+        Assert.All(strategy.WithSeed(1UL).Sample(100), v => Assert.InRange(v, (byte)0, (byte)10));
     }
 }
