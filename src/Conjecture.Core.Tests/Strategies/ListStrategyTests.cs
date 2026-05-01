dff4e3d5f84f9919f41294706c5cab81ffee510f
Kim Ommundsen <kommundsen@users.noreply.github.com>
2026-05-01 17:44:21 +0200

    Converts ListStrategyTests to use public Sample API and [Property]

--- src/Conjecture.Core.Tests/Strategies/ListStrategyTests.cs
+++ src/Conjecture.Core.Tests/Strategies/ListStrategyTests.cs
@@ -1,88 +1,67 @@
 // Copyright (c) 2026 Kim Ommundsen. Licensed under the MPL-2.0.
 // See LICENSE.txt in the project root or https://mozilla.org/MPL/2.0/
 
+using System.Collections.Generic;
+using System.Linq;
+
 using Conjecture.Core;
-using Conjecture.Core.Internal;
 
 namespace Conjecture.Core.Tests.Strategies;
 
 public class ListStrategyTests
 {
-    private static ConjectureData MakeData(ulong seed = 42UL) =>
-        ConjectureData.ForGeneration(new SplittableRandom(seed));
-
     [Fact]
     public void Lists_ProducesListOfCorrectType()
     {
-        var strategy = Strategy.Lists(Strategy.Integers<int>());
-        var result = strategy.Generate(MakeData());
-        Assert.IsType<List<int>>(result);
+        Strategy<List<int>> strategy = Strategy.Lists(Strategy.Integers<int>());
+        Assert.IsType<List<int>>(strategy.Sample());
     }
 
     [Fact]
     public void Lists_DefaultSizeVariesAcrossSeeds()
     {
-        var strategy = Strategy.Lists(Strategy.Integers<int>());
-        var sizes = new HashSet<int>();
-        for (var i = 0; i < 200; i++)
-        {
-            sizes.Add(strategy.Generate(MakeData((ulong)i)).Count);
-        }
+        Strategy<List<int>> strategy = Strategy.Lists(Strategy.Integers<int>());
+        IReadOnlyList<List<int>> samples = strategy.WithSeed(0UL).Sample(200);
+        HashSet<int> sizes = samples.Select(l => l.Count).ToHashSet();
         Assert.True(sizes.Count > 5, "List sizes should vary across seeds");
     }
 
     [Fact]
     public void Lists_DefaultSizeWithinRange()
     {
-        var strategy = Strategy.Lists(Strategy.Integers<int>());
-        for (var i = 0; i < 200; i++)
-        {
-            var count = strategy.Generate(MakeData((ulong)i)).Count;
-            Assert.InRange(count, 0, 100);
-        }
+        Strategy<List<int>> strategy = Strategy.Lists(Strategy.Integers<int>());
+        Assert.All(strategy.WithSeed(0UL).Sample(200), l => Assert.InRange(l.Count, 0, 100));
     }
 
     [Fact]
     public void Lists_RespectsMinSizeAndMaxSize()
     {
-        var strategy = Strategy.Lists(Strategy.Integers<int>(), minSize: 3, maxSize: 5);
-        for (var i = 0; i < 100; i++)
-        {
-            var count = strategy.Generate(MakeData((ulong)i)).Count;
-            Assert.InRange(count, 3, 5);
-        }
+        Strategy<List<int>> strategy = Strategy.Lists(Strategy.Integers<int>(), minSize: 3, maxSize: 5);
+        Assert.All(strategy.WithSeed(0UL).Sample(100), l => Assert.InRange(l.Count, 3, 5));
     }
 
     [Fact]
     public void Lists_EmptyListPossibleWhenMinSizeIsZero()
     {
-        var strategy = Strategy.Lists(Strategy.Integers<int>(), minSize: 0, maxSize: 10);
-        var seenEmpty = false;
-        for (var i = 0; i < 500; i++)
-        {
-            if (strategy.Generate(MakeData((ulong)i)).Count == 0)
-            {
-                seenEmpty = true;
-                break;
-            }
-        }
-        Assert.True(seenEmpty, "Empty list should be possible when minSize=0");
+        Strategy<List<int>> strategy = Strategy.Lists(Strategy.Integers<int>(), minSize: 0, maxSize: 10);
+        IReadOnlyList<List<int>> samples = strategy.WithSeed(0UL).Sample(500);
+        Assert.Contains(samples, l => l.Count == 0);
     }
 
     [Fact]
     public void Lists_DeterministicWithSameSeed()
     {
-        var strategy = Strategy.Lists(Strategy.Integers<int>());
-        var list1 = strategy.Generate(MakeData(99UL));
-        var list2 = strategy.Generate(MakeData(99UL));
+        Strategy<List<int>> strategy = Strategy.Lists(Strategy.Integers<int>());
+        List<int> list1 = strategy.WithSeed(99UL).Sample();
+        List<int> list2 = strategy.WithSeed(99UL).Sample();
         Assert.Equal(list1, list2);
     }
 
     [Fact]
     public void Lists_ElementsComefromInnerStrategy()
     {
-        var strategy = Strategy.Lists(Strategy.Integers<int>(min: 7, max: 7), minSize: 5, maxSize: 5);
-        var result = strategy.Generate(MakeData());
+        Strategy<List<int>> strategy = Strategy.Lists(Strategy.Integers<int>(min: 7, max: 7), minSize: 5, maxSize: 5);
+        List<int> result = strategy.Sample();
         Assert.All(result, x => Assert.Equal(7, x));
     }
 }
